b2dc3411f99bf3a680e553332956520452c769c2
Chris Pikul <chris@workmaple.com>
2023-11-02 22:02:36 +0300

    ready for version 1

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -8,16 +8,19 @@ import (
 
 func main() {
 	// Build a tree
-	doc := htmg.NewElem("html", htmg.NewAttr("lang", "en")).Append(
-		htmg.NewElemWithChildren(
-			"body",
-			htmg.NewElem("h1").Append(
-				htmg.NewText("Hello, World!"),
+	doc := htmg.NewDoc(
+		htmg.NewString("<!DOCTYPE html>"),
+		htmg.NewElem("html", htmg.NewAttr("lang", "en")).Append(
+			htmg.NewElemWithChildren(
+				"body",
+				htmg.NewElem("h1").Append(
+					htmg.NewText("Hello, World!"),
+				),
+				htmg.NewText("I'm HTMG from Novafex"),
 			),
-			htmg.NewText("I'm HTMG from Novafex"),
-		),
+		).(*htmg.Elem),
 	)
 
 	doc.Write(os.Stdout)
-	// <html lang="en"><body><h1>Hello, World!</h1>I'm HTMG from Novafex</body></html>
+	// <!DOCTYPE html><html lang="en"><body><h1>Hello, World!</h1>I'm HTMG from Novafex</body></html>
 }
